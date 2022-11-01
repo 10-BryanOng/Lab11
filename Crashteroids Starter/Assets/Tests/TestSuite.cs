475154980a819e605288b58fd3db1776b8caf538
10-BryanOng <bryanong0102@yahoo.com.sg>
2022-11-01 10:57:30 +0800

    Added Setup and Teardown
    Added Setup and Tear down test

---
+++ Crashteroids Starter/Assets/Tests/TestSuite.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator AsteroidsMoveDown()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            float initialYPos = asteroid.transform.position.y;
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.Less(asteroid.transform.position.y, initialYPos);
+        }
+
+        [UnityTest]
+        public IEnumerator GameOverOccursOnAsteroidCollision()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = game.GetShip().transform.position;
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.True(game.isGameOver);
+        }
+    }
+
+}
